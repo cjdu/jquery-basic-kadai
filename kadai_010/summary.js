3c6234a71762b1af62e4a71cd6350794eeed3716
cjdu <jssdbo1277@yahoo.co.jp>
2024-05-16 22:43:57 +0900

    最初のコミット

--- kadai_010/summary.js
+++ kadai_010/summary.js
@@ -1,1 +1,23 @@
+$(function() {
 
+  $('#change-color').on({
+    'click': () => {
+      $('P').css('color', 'blue');
+    }
+  });
+
+  $('#change-text').on('click', function() {
+    $('p').text('いい天気ですね');
+  });
+
+  $('#fade-out').on('click', function() {
+    $('p').fadeOut(3000, function() {
+    }); 
+  });
+
+  $('#fade-in').on('click', function() {
+    $('p').fadeIn(3000, function() {
+    }); 
+  });
+
+});
